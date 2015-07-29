bdee8bd15ea605cd9a70de422fa0c5634be5ab23
Sarath N K <sarathnk33@gmail.com>
2015-07-29 15:22:00 +0530

    Update FrameLayoutFixed.java

--- Audio/src/com/example/audio/FrameLayoutFixed.java
+++ Audio/src/com/example/audio/FrameLayoutFixed.java
@@ -1,10 +1,4 @@
-/*
- * This is the source code of Telegram for Android v. 1.3.2.
- * It is licensed under GNU GPL v. 2 or later.
- * You should have received a copy of the license in this archive (see LICENSE).
- *
- * Copyright Nikolai Kudashov, 2013.
- */
+
 
 package com.example.audio;
 
